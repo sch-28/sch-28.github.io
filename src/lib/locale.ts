e013eedc51b785c9a7e75689a1a30bab2238587c
jan <jansch2803@googlemail.com>
2024-08-25 01:08:24 +0200

    fix: ssr

--- src/lib/locale.ts
+++ src/lib/locale.ts
@@ -1,14 +1,15 @@
 import { derived, writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
 export type Locale = 'en' | 'de';
 export type Translation = Record<Locale, string>;
 export const locales: Locale[] = ['en', 'de'];
 
-const initialLocale = localStorage.getItem('locale') as Locale;
+const initialLocale: Locale = browser ? (localStorage.getItem('locale') as Locale) : 'en';
 export const locale = writable<Locale>(initialLocale);
 locale.subscribe((value) => {
 	const newLocale = value === 'de' ? 'de' : 'en';
-	localStorage.setItem('locale', newLocale);
+	browser && localStorage.setItem('locale', newLocale);
 });
 
 function translate(locale: Locale, translations: Translation) {
--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,10 +1,12 @@
 import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
 export type Theme = 'dark' | 'light';
 
-const initialTheme = localStorage.getItem('theme') as Theme;
+const initialTheme = browser ? (localStorage.getItem('theme') as Theme) : 'dark';
 export const theme = writable<Theme>(initialTheme);
 theme.subscribe((value) => {
+	if (!browser) return;
 	const newTheme = value === 'dark' ? 'dark' : 'light';
 	localStorage.setItem('theme', newTheme);
 	document.documentElement.classList.remove('dark', 'light');
